dd65bb375dc94701536fa1948fe8d93ecc5f5c8f
brktrkmn <brkturkmen@gmail.com>
2021-07-28 18:15:31 -0400

    adding css styling and effects

--- js/beerInfo.js
+++ js/beerInfo.js
@@ -1,40 +1,46 @@
-let water = document.querySelector('#water')
-let barley = document.querySelector('#barley')
-let hops = document.querySelector('#hops')
-let yeast = document.querySelector('#yeast')
-let clickWater = document.querySelector('.miniIconBoxWater')
-let clickBarley = document.querySelector('.miniIconBoxBarley')
-let clickHops = document.querySelector('.miniIconBoxHop')
-let clickYeast = document.querySelector('.miniIconBoxYeast')
-const backButtons =document.querySelectorAll('.btn')
+(() => {
+  // const icons = document.querySelectorAll(".iconContainer"),
+  let waterBox = document.querySelector(".miniIconBoxWater"),
+      barleyBox = document.querySelector(".miniIconBoxBarley"),
+      hopBox = document.querySelector(".miniIconBoxHop"),
+      yeastBox = document.querySelector(".miniIconBoxYeast");
+  const lBox = document.querySelector(".lightbox"),
+        lbClose = lBox.querySelector(".close-button"),
+        infoHeader = document.querySelector(".infoHeader"),
+        beerInfo = document.querySelector(".beerInfo");
 
-function waterInfo() {
-  water.classList.remove('hidden');
-}
+  //store beer info headers
+  const beerData = [
+    ["Water", `We use pure water from ancient aquifers, deep lakes and Rocky Mountain springs to make a more refreshing beer.`],
+    ["Barley", `We use spring and winter barley varieties to keep your beer fresh all year. Our barley is purchased direcly from the organic barley producers in Ontario.`],
+    ["Hop",`Our "Citra" hops come from Washington. Tasting notes are primarily lemon and grass, with some other lightly sweet flavors thrown into the mix like lime and melon. Citra is fresh, light and sweet.`],
+    ["Yeast", `Brewing yeast is the most essential of the four beer ingredients. Without it, we would just have bitter grain water that never fermented. We know that the correct strain of brewing yeast it critical to a perfect fermentation that's why we carry a wide variety of lager yeast from organic yeast producers in Ontario.`],
+  ]
 
-function barleyInfo() {
-  barley.classList.remove('hidden');
-}
 
-function hopsInfo() {
-  hops.classList.remove('hidden');
-}
 
-function yeastInfo() {
-  yeast.classList.remove('hidden');
-}
+  function popLightBox() {
+    showInfo(this.dataset.offset);
+    lBox.classList.add('show-lightbox');
 
-function backBtn() {
-  water.classList.add('hidden');
-  barley.classList.add('hidden');
-  hops.classList.add('hidden');
-  yeast.classList.add('hidden');
-}
+  }
 
-clickWater.addEventListener("click", waterInfo, false)
-clickBarley.addEventListener("click", barleyInfo, false)
-clickHops.addEventListener("click", hopsInfo, false)
-clickYeast.addEventListener("click", yeastInfo, false)
-for (let btn of backButtons) {
-  btn.addEventListener("click", backBtn, false)
-}
+  function showInfo(targetIndex) {
+    infoHeader.textContent = beerData[targetIndex][0];
+    beerInfo.textContent = beerData[targetIndex][1];
+
+  }
+  function closeLightBox() {
+    lBox.classList.remove('show-lightbox');
+  }
+
+  waterBox.addEventListener("click", popLightBox);
+  barleyBox.addEventListener("click", popLightBox);
+  hopBox.addEventListener("click", popLightBox);
+  yeastBox.addEventListener("click", popLightBox);
+
+  lbClose.addEventListener("click", closeLightBox);
+
+
+
+})();
